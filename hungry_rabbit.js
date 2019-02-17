583742d468a39de3270aa9d84591606923aaaff5
Emma Deas <emmadeas9@gmail.com>
2019-02-17 15:46:45 -0800

    clean up code

--- hungry_rabbit.js
+++ hungry_rabbit.js
@@ -20,8 +20,8 @@
 // 27
 
 const hungryRabbit = function (matrix) {
-    let pos = findStartPos(matrix);
-    return findNext(matrix, pos[0], pos[1]);
+    let [row, col] = findStartPos(matrix);
+    return findNext(matrix, row, col);
 };
 
 
@@ -54,12 +54,14 @@ const findNext = function (matrix, row, col) {
     let next_row = null;
     let next_col = null;
     moves.forEach(move => {
-        if (row + move[0] >= 0 && row + move[0] < matrix[row].length) {
-            if (col + move[1] >= 0 && col + move[1] < matrix.length) {
-                if (max < matrix[(row + move[0])][(col + move[1])]) {
-                    max = matrix[(row + move[0])][(col + move[1])];
-                    next_row = row + move[0];
-                    next_col = col + move[1];
+        let row_op = row + move[0];
+        let col_op = col + move[1];
+        if (row_op >= 0 && row_op < matrix[row].length) {
+            if (col_op >= 0 && col_op < matrix.length) {
+                if (max < matrix[row_op][col_op]) {
+                    max = matrix[row_op][col_op];
+                    next_row = row_op;
+                    next_col = col_op;
                 }
             }
         }
